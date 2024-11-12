09f21ff24fe40cf7409f2ac7567b56e13a374453
Yuriko Inaba <yurikoainaba@gmail.com>
2024-11-12 18:42:47 -0500

    Specify versions

--- src/e2e-tests/services/anvil/entrypoint.sh
+++ src/e2e-tests/services/anvil/entrypoint.sh
@@ -35,7 +35,7 @@ fi
 
 # Install Solidity dependencies
 echo "Installing Solidity dependencies..."
-forge install OpenZeppelin/openzeppelin-contracts@v5.0.2 --no-commit
+forge install OpenZeppelin/openzeppelin-contracts@v5.1.0 --no-commit
 
 # Copy contracts to the src directory
 echo "Copying contracts to the src directory..."
